eecd94472f3d2ea90e8a570357438715c49812ab
leandro silveira <leandrobhmgg@gmail.com>
2020-09-02 22:49:24 -0300

    event store

---
+++ Core/Person/DomainEvents/PersonCreated.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tactical.DDD;
+
+namespace Core.Person.DomainEvents
+{
+    public class PersonCreated : DomainEvent
+    {
+        public string PersonId { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public PersonCreated(
+            string personId, 
+            string firstName, 
+            string lastName)
+        {
+            PersonId = personId;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ RestAPI/Model/GeneratePersonDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RestAPI.Model
+{
+    public class GeneratePersonDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
